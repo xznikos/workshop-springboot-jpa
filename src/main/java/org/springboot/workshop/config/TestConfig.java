9494a58847790d9f732c30f8154ad50a006de5df
xznikos <nikolasmota2@gmail.com>
2026-02-04 03:52:44 -0300

    Order, Instant, ISO 8601

--- src/main/java/org/springboot/workshop/config/TestConfig.java
+++ src/main/java/org/springboot/workshop/config/TestConfig.java
@@ -1,12 +1,15 @@
 package org.springboot.workshop.config;
 
+import org.springboot.workshop.entities.Order;
 import org.springboot.workshop.entities.User;
+import org.springboot.workshop.repositories.OrderRepository;
 import org.springboot.workshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,11 +19,21 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
         User u1 = new User(null, "Joao", "joao@.com", "999999", "12345");
         User u2 = new User(null, "Amanda", "amanda@.com", "999999", "12345");
 
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
---
+++ src/main/java/org/springboot/workshop/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.springboot.workshop.repositories;
+
+import org.springboot.workshop.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
